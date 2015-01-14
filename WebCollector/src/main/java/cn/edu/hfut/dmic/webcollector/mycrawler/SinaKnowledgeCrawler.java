6e6858ac5856e2c9778c5e45217984b3030350c0
longyan_jerry@foxmail.com <longyan_jerry@foxmail.com>
2015-01-15 00:01:39 +0800

    s

--- WebCollector/src/main/java/cn/edu/hfut/dmic/webcollector/mycrawler/SinaKnowledgeCrawler.java
+++ WebCollector/src/main/java/cn/edu/hfut/dmic/webcollector/mycrawler/SinaKnowledgeCrawler.java
@@ -41,26 +41,46 @@ public class SinaKnowledgeCrawler extends DeepCrawler {
         partAge.put("/yeq/",50);//婴儿期
         partAge.put("/yeq1/",60);//幼儿期
         partAge.put("/xlq/",70);//学龄前
-        partCategory.put("//",1);//
-        partCategory.put("//",1);//
-        partCategory.put("//",1);//
-        partCategory.put("//",1);//
-        partCategory.put("//",1);//
-        partCategory.put("//",1);//
-        partCategory.put("//",1);//
-        partCategory.put("//",1);//
-        partCategory.put("//",1);//
-        partCategory.put("//",1);//
-        partCategory.put("//",1);//
-        partCategory.put("//",1);//
-        partCategory.put("//",1);//
-        partCategory.put("//",1);//
-        partCategory.put("//",1);//
-        partCategory.put("//",1);//
-        partCategory.put("//",1);//
-        partCategory.put("//",1);//
-        partCategory.put("//",1);//
-        partCategory.put("//",1);//
+
+        partCategory.put("/qt/",0);//其他
+        partCategory.put("/hqzb1/",1);//孕前准备
+        partCategory.put("/bylc/",2);//避孕流产
+        partCategory.put("/hyjj/",3);//怀孕禁忌
+        partCategory.put("/ycys/",4);//遗传优生
+        partCategory.put("/snsn/",5);//生男生女
+        partCategory.put("/byby/",6);//不孕不育
+        partCategory.put("/yyys/",7);//营养饮食
+        partCategory.put("/tefy/",8);//胎儿发育
+        partCategory.put("/yqbj/",9);//孕期保健
+        partCategory.put("/yqjc/",10);//孕期检查
+        partCategory.put("/yqxl/",11);//孕期心理
+        partCategory.put("/ssyf/",12);//时尚孕妇
+        partCategory.put("/cjjb/",13);//常见疾病
+        partCategory.put("/yqaq/",14);//孕期安全
+        partCategory.put("/tj2/",15);//胎教
+        partCategory.put("/dbt/",16);//多胞胎
+        partCategory.put("/scfs/",17);//生产方式
+        partCategory.put("/lpdc/",18);//临盆待产
+        partCategory.put("/fmgc/",19);//分娩过程
+        partCategory.put("/yjbb/",20);//迎接宝贝
+        partCategory.put("/mrwy/",21);//母乳喂养
+        partCategory.put("/chhf/",22);//产后恢复
+        partCategory.put("/chyyz/",23);//产后抑郁症
+        partCategory.put("/aqbj/",24);//安全保健
+        partCategory.put("/cczb/",25);//成长指标
+        partCategory.put("/rgwy/",26);//人工喂养
+        partCategory.put("/hlbj/",27);//护理保健
+        partCategory.put("/myjz/",28);//免疫接种
+        partCategory.put("/qnkf/",29);//潜能开发
+        partCategory.put("/zce/",30);//早产儿
+        partCategory.put("/jtjy/",31);//家庭教育
+        partCategory.put("/xlxwyy/",32);//心理行为语言
+        partCategory.put("/yspy/",33);//艺术培养
+        partCategory.put("/yy4/",34);//英语
+        partCategory.put("/yd1/",35);//阅读
+        partCategory.put("/wj3/",36);//玩具
+        partCategory.put("/xxjy/",37);//学校教育
+        partCategory.put("/yey/",38);//幼儿园
     }
 
     @Override
@@ -71,7 +91,7 @@ public class SinaKnowledgeCrawler extends DeepCrawler {
             Links nextLinks=new Links();
             /*将所有搜索结果条目的超链接返回，爬虫会在下一层爬取中爬取这些链接*/
             nextLinks.addAllFromDocument(page.getDoc(), regexRule);
-            Integer age = 80;//其他年龄
+            Integer age = 0;//其他年龄
             for(String part : partAge.keySet()){
                 if(url.contains(part)){
                     age = partAge.get(part);
